1d6f8733dcc92388b014ce767401c4198642c189
Jardel Kuhn <jardelkuhn@gmail.com>
2023-07-20 22:14:40 -0300

    great_places project start

---
+++ lib/screens/places.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: [],
+      ),
+    );
+  }
+}
